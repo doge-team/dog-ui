7beaa42b55d8ed8271d8ede6a3f71af075e9f31b
qiuzijie <771447612@qq.com>
2022-05-30 18:00:09 +0800

    add devServer config

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,15 @@
+/*
+ * @Author: qiuzijie 771447612@qq.com
+ * @Date: 2022-05-27 16:00:52
+ * @LastEditors: qiuzijie 771447612@qq.com
+ * @LastEditTime: 2022-05-30 12:42:25
+ * @FilePath: \dog-ui\vue.config.js
+ * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+ */
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
-  transpileDependencies: true
+  transpileDependencies: true,
+  devServer: {
+    allowedHosts: ['.natappfree.cc']
+  }
 })
