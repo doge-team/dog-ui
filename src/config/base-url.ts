aef2f76907a7e4e5663f61bfd1513e532919d8ae
qiuzijie <771447612@qq.com>
2022-06-06 17:28:23 +0800

    docker部署优化

--- src/config/base-url.ts
+++ src/config/base-url.ts
@@ -8,7 +8,7 @@
  */
 export const BaseUrlConfig = {
     development: {
-      baseUrl: 'http://q9vxaj.natappfree.cc' // 测试接口域名
+      baseUrl: 'http://tatm6a.natappfree.cc/' // 测试接口域名
     },
     beta: {
       baseUrl: 'http://xxx.xxx.xxx.xxx:xxxx' // 测试接口域名
@@ -17,6 +17,6 @@ export const BaseUrlConfig = {
       baseUrl: 'http://xxx.xxx.xxx.xxx:xxxx' // 正式接口域名
     },
     production: {
-      baseUrl: 'http://xxx.xxx.xxx.xxx:xxxx'
+      baseUrl: 'http://tatm6a.natappfree.cc/'
     }
   }
