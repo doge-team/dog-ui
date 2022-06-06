d903b4575949339c1adb4897e2558128ca1a8d8c
qiuzijie <771447612@qq.com>
2022-06-06 09:06:51 +0800

    add login

---
+++ src/views/admin/login/login.vue
@@ -0,0 +1,28 @@
+<template>
+    <div class="login-container">
+        <div class="login-box">
+            <div class="img"></div>
+            <div class="login-form">
+                <p>登录</p>
+                <el-row>
+                    <el-input
+                        v-model="input2"
+                        class="w-50 m-2"
+                        placeholder="Type something"
+                        :prefix-icon="Search"
+                    />
+                </el-row>
+                <el-row></el-row>
+            </div>
+
+        </div>
+    </div>
+</template>
+<script>
+export default {
+    
+}
+</script>
+<style lang="less">
+    
+</style>
