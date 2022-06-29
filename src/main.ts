72a75d36f6e8adc84a78e03735fab3335c990e07
Pluto1243 <1243173286@qq.com>
2022-06-29 18:27:00 +0800

    pref: window resize & page adjust

--- src/main.ts
+++ src/main.ts
@@ -10,6 +10,16 @@ import './permission'
 
 const app = createApp(App)
 
+// resize for scaling the board size
+window.addEventListener('resize', onResize)
+// set size on startup
+onResize()
+
+function onResize() {
+    // get actual vh on mobile
+    document.body.style.setProperty('--vh', window.innerHeight + 'px')
+}
+
 app.use(ElementPlus)
     .use(router);
 app.mount('#app')
