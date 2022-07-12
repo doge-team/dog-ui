6557a3a720dcc017a5faada92a56983f17a626d4
Pluto1243 <1243173286@qq.com>
2022-07-12 17:35:28 +0800

    feat: test

--- src/config/base-url.ts
+++ src/config/base-url.ts
@@ -5,6 +5,7 @@
  * @LastEditTime: 2022-06-01 10:27:23
  * @FilePath: \dog-ui\src\config\base-url.ts
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+ * test
  */
 export const BaseUrlConfig = {
     development: {
