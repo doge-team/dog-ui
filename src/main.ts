b52e988ab119ebf0cc78614f4a9aee1f765b1f42
qiuzijie <771447612@qq.com>
2022-07-15 09:56:32 +0800

    docker file chagne

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,11 @@
-import { createApp, devtools } from 'vue'
-import ElementPlus from 'element-plus'
-import 'element-plus/dist/index.css'
-import '@/style/index.less';
 import '@/assets/font/font.less';
-import App from './App.vue'
-import router from './router'
-import vue from 'vue'
-import './permission'
+import '@/style/index.less';
+import ElementPlus from 'element-plus';
+import 'element-plus/dist/index.css';
+import { createApp } from 'vue';
+import App from './App.vue';
+import './permission';
+import router from './router';
 
 const app = createApp(App)
 
